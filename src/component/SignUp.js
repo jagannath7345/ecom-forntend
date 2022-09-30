dfeaca530b03c7ad0126080ff3d37a7a1ee929fa
Jagannath <guptajn7345@gmail.com>
2022-09-30 14:03:12 +0530

    signup page

--- src/component/SignUp.js
+++ src/component/SignUp.js
@@ -1,15 +1,44 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const SignUp = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitData = () => {
+    console.log(name,email,password)
+  }
+
   return (
     <div className="ragister">
-  <h1>Ragister</h1>
-  <input className='inputBox' type="text" placeholder='Enter Name' />
-  <input className='inputBox' type="email" placeholder='Enter Email' />
-  <input className='inputBox' type="password" placeholder='Enter Password' />
-  <button className='buttonReg' type="button"> Ragister</button>
+      <h1>Ragister</h1>
+      <input
+        className="inputBox"
+        type="text"
+        placeholder="Enter Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        className="inputBox"
+        type="email"
+        placeholder="Enter Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        className="inputBox"
+        type="password"
+        placeholder="Enter Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button className="buttonReg" type="button"onClick={submitData}>
+        {" "}
+        Ragister
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
